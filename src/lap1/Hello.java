b11006db2c5611e555fba9470dd7d093d52c8834
yuy <yuyna120@gmail.com>
2019-12-11 09:23:10 +0700

    Initial commit

---
+++ src/lap1/Hello.java
@@ -0,0 +1,15 @@
+package lap1;
+
+public class Hello {
+    public static void main (String[] args){
+
+        System.out.println("Hello World,I am yuy");
+        System.out.println("MIT");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("saiyai");
+        System.out.println("Thungsong");
+        System.out.println("Thailand");
+
+
+    }//main
+}//class
